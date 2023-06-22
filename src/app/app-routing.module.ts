adf0f7c132e35c7cccc890501e21e91793d79628
Rafael <rafaelaires.acs@gmail.com>
2023-06-22 14:39:03 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'cadastro',component:CadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
