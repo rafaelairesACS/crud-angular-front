37f1b2add9ccfae159d5eb3c2586effc96414ecb
rafadot <rafaelairesdemedeiros@outlook.com>
2023-06-22 21:15:51 -0300

    form cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 
 const routes: Routes = [
-  {path:'login',component:LoginComponent},
+  {path:'',component:LoginComponent},
   {path:'cadastro',component:CadastroComponent}
 ];
 
